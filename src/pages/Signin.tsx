268b95c47ca856215b5c035bff8ec4a9550f8561
Sushant Mishra <150696525+mishrasushant101@users.noreply.github.com>
2025-01-20 18:54:38 +0530

    Added a few chanves to frontend

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,27 +1,44 @@
+import { useRef } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import { Navbar } from "../components/Navbar";
 import { BrainIcon } from "../icons/BrainIcon";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 export function Signin() {
+    const usernameRef = useRef<HTMLInputElement>();
+    const passwordRef = useRef<HTMLInputElement>();
     const navigate = useNavigate();
- 
-        const goToSignup = () => {
-            navigate("/signup");
-        };
+
+    async function signin() {
+        const username = usernameRef.current?.value;
+        const password = passwordRef.current?.value;
+        const response = await axios.post(BACKEND_URL + "/api/v1/signin", {
+            username,
+            password
+        })
+        const jwt = response.data.token;
+        localStorage.setItem("token", jwt);
+        navigate("/dashboard")
+    }
+
+    const goToSignup = () => {
+        navigate("/signup");
+    };
     return <div>
         <Navbar />
         <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
             <div className="bg-white rounded-xl border min-w-48 p-8 ">
                 <div className="flex justify-center mb-4 text-xl font-semibold">
-                                <BrainIcon size="sm" />
-                                <span>Second Brain</span>
-                                </div>
-                <Input placeholder="username" />
-                <Input placeholder="password" />
+                    <BrainIcon size="sm" />
+                    <span>Second Brain</span>
+                </div>
+                <Input reference={usernameRef} placeholder="username" />
+                <Input reference={passwordRef} placeholder="password" />
                 <div className="flex justify-center pt-4">
-                    <Button variant="primary" text="Signin" />
+                    <Button onClick={signin} variant="primary" text="Signin" />
                 </div>
                 <div className="pt-4 flex justify-center">
                     <span>Don't have an account? <button className="text-purple-600" onClick={goToSignup}>signup</button></span>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,27 +1,43 @@
+import { useRef } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import { Navbar } from "../components/Navbar";
 import { BrainIcon } from "../icons/BrainIcon";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 export function Signup() {
+    const usernameRef = useRef<HTMLInputElement>();
+    const passwordRef = useRef<HTMLInputElement>();
     const navigate = useNavigate();
- 
-        const goToSignin = () => {
-            navigate("/signin");
-        };
+
+    async function signup() {
+        const username = usernameRef.current?.value;
+        const password = passwordRef.current?.value;
+        await axios.post(BACKEND_URL + "/api/v1/signup", {
+            username,
+            password
+        })
+        navigate("/signin")
+        alert("you have signed up")
+    }
+
+    const goToSignin = () => {
+        navigate("/signin");
+    };
     return <div>
         <Navbar />
         <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
             <div className="bg-white rounded-xl border min-w-48 p-8 ">
                 <div className="flex justify-center mb-4 text-xl font-semibold">
-                <BrainIcon size="sm" />
-                <span>Second Brain</span>
+                    <BrainIcon size="sm" />
+                    <span>Second Brain</span>
                 </div>
-                <Input placeholder="username" />
-                <Input placeholder="password" />
+                <Input reference={usernameRef} placeholder="username" />
+                <Input reference={passwordRef} placeholder="password" />
                 <div className="flex justify-center pt-4">
-                    <Button variant="primary" text="Signup" />
+                    <Button onClick={signup} variant="primary" text="Signup" />
                 </div>
                 <div className="pt-4 flex justify-center">
                     <span>Already have an account? <button className="text-purple-600" onClick={goToSignin}>signin</button></span>
